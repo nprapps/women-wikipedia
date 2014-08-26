6abea2e4e9278baa884e23f7b3bd51515d4b4f50
Tyler Fisher <tyler@tylerjfisher.com>
2014-08-26 13:12:51 -0400

    haven't committed in a while

--- scraper.py
+++ scraper.py
@@ -9,19 +9,20 @@ from slugify import slugify
 import unicodecsv
 
 BASE_URL = 'https://en.wikipedia.org/'
-WOMEN_LIST = 'http://en.wikipedia.org/wiki/List_of_female_governors_in_the_United_States'
-CSV_PATH = 'women.csv'
+BASE_LIST = 'http://en.wikipedia.org/wiki/List_of_former_members_of_the_United_States_House_of_Representatives'
+CSV_PATH = 'all_representatives.csv'
+LETTERS = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','Y','Z']
 
 s = Scraper(requests_per_minute=60)
 s.cache_storage = FileCache('wikipedia_cache')
 s.cache_write_only = False
 
 
-def write_corpus():
-    response = s.urlopen(WOMEN_LIST)
+def write_corpus(list):
+    response = s.urlopen(list)
     doc = fromstring(response)
     table = doc.find_class('wikitable')[0]
-    writer = unicodecsv.writer(open(CSV_PATH, 'w'))
+    writer = unicodecsv.writer(open('tmp/%s' % CSV_PATH, 'w'))
     writer.writerow(['first_name', 'last_name', 'wiki_url'])
     for row in table.findall('tr'):
         write_row(row, writer)
@@ -30,22 +31,27 @@ def write_corpus():
 def write_row(row, writer):
     tds = row.findall('td')
     try:
-        name_cell = tds[1]
-        name = tds[1].text_content()
+        name_cell = tds[0]
+        name_anchor = name_cell.findall('a')[0]
+        name = name_anchor.text_content()
 
         # split first name and last name. not worrying about middles
         name_splits = name.split(' ')
         first_name = name.split(' ')[0]
+
         last_name = name.split(' ')[len(name_splits) - 1]
+        # ugh, Jr., etc.
+        if last_name == 'Jr.' or last_name == 'Sr.':
+            last_name = name.split(' ')[len(name_splits) - 2].split(',')[0]
 
-        # there are italics in this table we have to account for
-        italics = name_cell.find('i')
-        if italics is not None:
-            anchor = italics.find('a')
-        else:
-            anchor = name_cell.find('a')
+        # # there are italics in this table we have to account for
+        # italics = span.find('i')
+        # if italics is not None:
+        #     anchor = italics.find('a')
+        # else:
+        #     anchor = span.find('span').find('a')
 
-        href = anchor.attrib['href']
+        href = name_anchor.attrib['href']
         writer.writerow([first_name, last_name, urljoin(BASE_URL, href)])
 
     except:
@@ -53,10 +59,12 @@ def write_row(row, writer):
 
 
 def read_csv():
-    with open(CSV_PATH, 'rb') as f:
+    with open('tmp/%s' % CSV_PATH, 'rb') as f:
         reader = csv.DictReader(f, fieldnames=["first_name", "last_name", "wiki_url"])
         for row in reader:
+            print row
             parse_wiki(row)
+
 
 
 def parse_wiki(row):
@@ -97,14 +105,23 @@ def grep_text(file, row):
         if row['last_name'] in line:
             last_name_count = last_name_count + 1
 
-    ratio = (float(first_name_count) / last_name_count)
+    if last_name_count != 0:
+        ratio = (float(first_name_count) / last_name_count)
+    else :
+        ratio = 0
 
     print row['first_name'], first_name_count
     print row['last_name'], last_name_count
     print 'ratio', ratio
     print '\n'
 
+    writer = unicodecsv.writer(open('output/%s' % CSV_PATH, 'a'))
+    writer.writerow([first_name_count, last_name_count, ratio])
+
 
 if __name__ == '__main__':
-    write_corpus()
-    read_csv()
+    for letter in LETTERS:
+        list = '%s_(%s)' % (BASE_LIST, letter)
+        print 'parsing %s' % list
+        write_corpus(list)
+        read_csv()
